1e941d30e08f0f054fd7bd62dbce153da99a6e5e
Morikomo <16456847+morikomo@user.noreply.gitee.com>
2026-04-10 23:29:29 +0800

    Add Player properties.

--- Player.h
+++ Player.h
@@ -1,4 +1,13 @@
 #pragma once
+#include <string>
+#include <vector>
+#include "PropertyTile.h"
 class Player {
+private:
+	std::string name;
+	int playerID;
+	int currentMoney;
+	int currentIndex;
+	std::vector<PropertyTile*> streets;
 };
 
